672e9795b3022e2af287f230ef4952e50836ef58
Miguel <miguelpegasus8@gmail.com>
2025-08-05 09:04:51 -0300

    Melhorias do sistema: Menu interativo

--- Program.cs
+++ Program.cs
@@ -2,24 +2,80 @@
 using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
-
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+bool working = true;
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+List<Pessoa> hospodes = new List<Pessoa>();
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 6, valorDiaria: 90);
+Suite suite1 = new Suite(tipoSuite: "Normal", capacidade: 4, valorDiaria: 60);
+Suite suite2 = new Suite(tipoSuite: "Kitnet", capacidade: 2, valorDiaria: 30);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 11);
 reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+while (working)
+{
+    Console.Clear();
+    Console.WriteLine("=== Hotelaria Silva & Co ===\n");
+    Console.WriteLine("1 - Cadastrar hóspedes");
+    Console.WriteLine("2 - Ver detalhes da reserva");
+    Console.WriteLine("3 - Sair\n");
+    Console.Write("Digite sua opção: ");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Console.Clear();
+            Console.WriteLine("Cadastro de Hóspedes");
+
+            hospodes.Clear();
+
+            Console.WriteLine("Quantos hospodes deseja cadastrar? ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                Console.WriteLine($"Digite o nome do hospode {i + 1}: ");
+                string nome = Console.ReadLine();
+                hospodes.Add(new Pessoa(nome));
+            }
+
+            reserva.CadastrarHospedes(hospodes);
+            Console.WriteLine("Hospodes cadastrados com sucesso!");
+
+            break;
+        case "2":
+            Console.Clear();
+            Console.WriteLine("Detalhes da reserva: \n");
+
+            Console.WriteLine($"Suíte: {suite.TipoSuite}");
+            Console.WriteLine($"Capacidade: {suite.Capacidade}");
+            Console.WriteLine($"Dias reservados: {reserva.DiasReservados}");
+            Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+
+            foreach (var c in hospodes)
+            {
+                Console.WriteLine($"- {c.Nome}");
+            }
+
+            Console.WriteLine($"Valor total da reserva: R${reserva.CalcularValorDiaria():F2}");
+
+            break;
+        case "3":
+            working = false;
+            break;
+        default:
+            Console.WriteLine("Opção iválida");
+            break;
+    }
+    if (working)
+    {
+        Console.WriteLine("\nPressione qualquer tecla para continuar...");
+        Console.ReadKey();
+    }
+}
+
+Console.WriteLine("O programa se encerrou\n"+ "Muito obrgado e até breve!");
